599e8882ae785fa06144a3ef0932fceb7db8ffdf
Carlos Bonadeo <carlosbonadeo@gmail.com>
2018-01-16 23:10:21 -0300

    Json server delay

--- src/components/Movimientos.vue
+++ src/components/Movimientos.vue
@@ -1,9 +1,10 @@
 <template>
     <b-container class="mt-4">
         <h3 class="mb-3">Movimientos</h3>
-        <b-button variant="primary" @click="fetch_movimientos">Actualizar movimientos</b-button>
-        <b-button variant="info" @click="RESET_MOVIMIENTOS">Estado anterior</b-button>
+        <b-button :disabled="cargando" variant="primary" @click="buscarMovimientos">Actualizar movimientos</b-button>
+        <b-button :disabled="cargando" variant="info" @click="RESET_MOVIMIENTOS">Estado anterior</b-button>
         <b-table class="mt-3" small hover striped :items="movimientos"></b-table>
+        <b-card v-show="cargando" class="text-center">Cargando movimientos</b-card>
     </b-container>
 </template>
 
@@ -12,9 +13,20 @@
 
     export default {
         name: 'Movimientos',
+        data() {
+            return {
+                cargando: false
+            }
+        },
         methods: {
             ...mapActions(['fetch_movimientos']),
             ...mapMutations(['RESET_MOVIMIENTOS']),
+            buscarMovimientos() {
+                this.cargando = true
+                this.fetch_movimientos().then(() => {
+                        this.cargando = false
+                    })
+            }
         },
         computed: {
             ...mapState(['movimientos'])
